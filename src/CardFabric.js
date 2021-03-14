d9de350b9257affe1139881d6b0c386486a07a4a
Vladislav <pickachu507@gmail.com>
2021-03-14 15:18:16 +0200

    some refactoring

--- src/CardFabric.js
+++ src/CardFabric.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card';
+import BasicCard from './BasicCard';
 import characters from "./characters.json";
 import "./CardFabric.css"
 
@@ -15,15 +15,15 @@ export class CardFabric extends React.Component {
     initializeCards() {
         for(var i in characters.cards) {
             this.cardRefs[i] = React.createRef();
-            this.cardComponents[i] =  <Card notify={this.notify} key={i} ref={this.cardRefs[i]} charInfo={characters.cards[i]}/>;
+            this.cardComponents[i] =  <BasicCard notify={this.notify} key={i} ref={this.cardRefs[i]} charInfo={characters.cards[i]}/>;
         }
     }
 
     notify = (sender, event) => {
-        if(event == "focus") {
+        if(event === "focus") {
             for(var i in this.cardRefs) {
                 if(sender !== this.cardRefs[i].current) {
-                    this.cardRefs[i].current.blur();
+                    this.cardRefs[i].current.execute["blur"]();
                 }
                 else {
                     this.setState({
@@ -35,19 +35,19 @@ export class CardFabric extends React.Component {
         }
     }
 
-    unfocus = () => {
+    handleClickBlocker = () => {
         for(var i in this.cardRefs) {
-            this.cardRefs[i].current.unblur();
+            this.cardRefs[i].current.execute["unblur"]();
         }
         document.getElementById("blocker").style.visibility = "hidden";
-        this.cardRefs[this.state.focusedCardIndex].current.unfocus();
+        this.cardRefs[this.state.focusedCardIndex].current.execute["unfocus"]();
     }
 
     render() {
         return (
             <div id="CardFabric">
                 {this.cardComponents}
-                <div onClick={this.unfocus} id="blocker"></div>
+                <div onClick={this.handleClickBlocker} id="blocker"></div>
             </div>
         );
     }
